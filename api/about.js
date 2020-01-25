d6f537705073a39787f7041623e8178f1d63c8eb
kryvokhyzha <kriwohizha@gmail.com>
2020-01-25 18:06:15 +0200

    Added about and contacts api

---
+++ api/about.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = async () => [
+    'Hi! I study at the third course of FICT KPI on "Information System and Technology" specialization. Now, I`m',
+    'looking for internship in Machine Learning or Data Science. I had already completed few ML and DL courses in',
+    'Coursera and Stepik, watched ods.ai and Deep Learning School lectures (all certificates you can find on my',
+    'LinkedIn page). Also, I have a few projects in web development on Python and NodeJS.'
+];
---
+++ api/contacts.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = async () => [
+    'Roman Kryvokhyzha',
+    'Telegram: @kryvokhyzha',
+    'E-mail: kriwohizha@gmail.com',
+    'LinkedIn: https://www.linkedin.com/in/kryvokhyzha/',
+    'GitHub: https://github.com/kryvokhyzha/'
+];
