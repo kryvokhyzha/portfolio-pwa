c6358773325d0f05f2b30c52a852ed3aa1348646
kryvokhyzha <kriwohizha@gmail.com>
2020-01-26 02:18:13 +0200

    Changed style

--- static/console.js
+++ static/console.js
@@ -101,12 +101,12 @@ const pad = (padChar, length) => new Array(length + 1).join(padChar);
 
 const isMobile = () => (
     navigator.userAgent.match(/Android/i) ||
-  navigator.userAgent.match(/webOS/i) ||
-  navigator.userAgent.match(/iPhone/i) ||
-  navigator.userAgent.match(/iPad/i) ||
-  navigator.userAgent.match(/iPod/i) ||
-  navigator.userAgent.match(/BlackBerry/i) ||
-  navigator.userAgent.match(/Windows Phone/i)
+    navigator.userAgent.match(/webOS/i) ||
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPod/i) ||
+    navigator.userAgent.match(/BlackBerry/i) ||
+    navigator.userAgent.match(/Windows Phone/i)
 );
 
 let viewportHeight, viewableRatio;
