5aa8cd1e4c6ca0e98be1c71cb1b75b752b91ea2a
kryvokhyzha <kriwohizha@gmail.com>
2020-01-26 04:08:17 +0200

    Added wrapper for api

--- api/contacts.js
+++ api/contacts.js
@@ -3,6 +3,6 @@
 module.exports = async () => [
     '📟 Telegram: @kryvokhyzha',
     '📭 E-mail: kriwohizha@gmail.com',
-    '<p>📖 LinkedIn: <a href="https://www.linkedin.com/in/kryvokhyzha/" target="_blank">Click on this</a> to open my LinkedIn page</p>',
-    '<p>📝 GitHub: <a href="https://github.com/kryvokhyzha/" target="_blank">Click on this</a> to open my GitHub profile</p>',
+    '📖 LinkedIn: <a href="https://www.linkedin.com/in/kryvokhyzha/" target="_blank">Click on this</a> to open my LinkedIn page',
+    '📝 GitHub: <a href="https://github.com/kryvokhyzha/" target="_blank">Click on this</a> to open my GitHub profile',
 ];
