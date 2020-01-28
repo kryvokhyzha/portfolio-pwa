274b285698d7596b4e4662c7b061efcb1e6fb4eb
kryvokhyzha <kriwohizha@gmail.com>
2020-01-28 16:15:40 +0200

    Added new API

---
+++ api/certificates.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = async () => [
+    '📜 Big Data School 4.0 - <i>Course by Kyivstart</i>',
+    '📜 Machine Learning - <i>Standford course by Andrew Ng`s</i>',
+    '📜 Введение в машинное обучение - <i>Course by High School of Economics University</i>',
+    '📜 Математика и Python для анализа данных - <i>Course by Yandex and MIPT</i>',
+    '📜 Обучение на размеченных данных - <i>Course by Yandex and MIPT</i>',
+    '📜 Поиск структуры в данных - <i>Course by Yandex and MIPT</i>',
+    '📜 Построение выводов по данным - <i>Course by Yandex and MIPT</i>',
+    '📜 Neural network and computer vision - <i>Course by Samsung R&D</i>'
+];
---
+++ api/projects.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = async () => [
+    '💡 Voice Quality Analyzer',
+    '<p style="margin-left: 4%">🔹 Language: Python</p>',
+    '<p style="margin-left: 4%">🔹 This project I made in Samsung R&D Institute Practicum.<br>' +
+    'The main idea is a build analyzer and detector for different type of noise in audio files.</p>',
+    '<br>',
+    '💡 Face Recognition',
+    '<p style="margin-left: 4%">🔹 Language: Python3, JS</p>',
+    '<p style="margin-left: 4%">🔹 This is my first course work. The main idea of this application was to<br>' +
+    'recognize wether people are preent on a live-stream<br> video on the lecture and record information about them in Excel file.</p>',
+    '<br>',
+    '💡 Remark',
+    '<p style="margin-left: 4%">🔹 Language: Java, TypeScript</p>',
+    '<p style="margin-left: 4%">🔹 Framework: SpringBoot, Angular</p>',
+    '<p style="margin-left: 4%">🔹 This project allows to create notebooks and notes for them before previous authorization. User can send feedback<br>' +
+    'about application via email and share notebooks with other users.</p>',
+    '<br>',
+    '💡 Fennec-fox-cast',
+    '<p style="margin-left: 4%">🔹 Language: JS, Node.JS</p>',
+    '<p style="margin-left: 4%">🔹 Fennec Fox is a peer-to-peer application, which allows to communicate via voice or video with your friends.</[]>'
+];
