0d1fa2da30260cea0666cd09e225a7075e11b1c9
kryvokhyzha <kriwohizha@gmail.com>
2020-01-29 21:56:48 +0200

    Improved auto-update

--- static/worker.js
+++ static/worker.js
@@ -11,7 +11,7 @@ const files = [
     '/Roman.png'
 ];
 
-const cacheName = 'v1.3.6.4';
+const cacheName = 'v1.3.6.8';
 
 self.addEventListener('install', event => event.waitUntil(
     caches.open(cacheName).then(cache => cache.addAll(files))
