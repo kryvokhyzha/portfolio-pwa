7bb266f90c5816030c657d33f9cd9715d7d2b4f6
kryvokhyzha <kriwohizha@gmail.com>
2020-01-31 01:44:09 +0200

    Changed code style

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo Starting deploy...
-docker login -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $DOCKER_USER/$DOCKER_APP_NAME:$TAG .
-docker push $DOCKER_USER/$DOCKER_APP_NAME:$TAG
+docker build -f Dockerfile -t "$DOCKER_USER"/"$DOCKER_APP_NAME":"$TAG" .
+docker push "$DOCKER_USER"/"$DOCKER_APP_NAME":"$TAG"
