e45efb80448f2c8afb80fe21618d2d85a7525e08
kryvokhyzha <kriwohizha@gmail.com>
2020-07-31 02:53:08 +0300

    Fixed text

--- static/worker.js
+++ static/worker.js
@@ -11,7 +11,7 @@ const files = [
     '/Roman.png'
 ];
 
-const cacheName = 'v1.4.6.8';
+const cacheName = 'v1.4.6.9';
 
 self.addEventListener('install', event => event.waitUntil(
     caches.open(cacheName).then(cache => cache.addAll(files))
