a40816cddaa5bbf6ced7b15e2f8e03776dab334f
kryvokhyzha <kriwohizha@gmail.com>
2022-01-14 22:44:03 +0200

    update layout

--- api/about.js
+++ api/about.js
@@ -1,22 +1,20 @@
 'use strict';
 
 module.exports = async () => [
-    '💡 Machine Learning Engineer at Kyivstar.',
+    '<p class="list-lamp-hyphens">💡 Machine Learning Engineer at Kyivstar.</p>',
     '<p class="list-hyphens">🔹 build antifraud and scoring models for financial organizations</p>',
     '<p class="list-hyphens">🔹 develop traffic cannibalization forecasting model using ML algorithms and Graph Neural Network</p>',
     '<p class="list-hyphens">🔹 develop custom automl tool</p>',
     '<p class="list-hyphens">🔹 work with a huge amount of data</p>',
-    '💡 Big Data Analyst at Kyivstar (from 17-02-2020 till 01-06-2021).',
+    '<p class="list-lamp-hyphens">💡 Big Data Analyst at Kyivstar (from 17-02-2020 till 01-06-2021).</p>',
     '<p class="list-hyphens">🔹 write Ad-hoc queries using Python, Hive, PySpak, and other big data tools and frameworks</p>',
     '<p class="list-hyphens">🔹 build look-alike models using ML-algorithms</p>',
     '<p class="list-hyphens">🔹 build a model to find the optimal location for store opening</p>',
     '<p class="list-hyphens">🔹 develop a system for monitoring data quality</p>',
     '<p class="list-hyphens">🔹 develop "silver" and "gold" data sources</p>',
-    '💡 Study at the fifth course of <a href="https://kau.org.ua/" target="_blank">KAU</a>, "Computer Science" specialization.',
-    '💡 Graduated with bachelor`s degree "Information System and Technologies" specialization, FICT, KPI.',
-    '💡 I have already completed few ML and DL courses in Coursera and Stepik,',
-    'watched ods.ai and Deep Learning School lectures. Also, I`m developing in Big Data,',
-    'learning Spark, PySpark, Scala, Kafka and so on.',
-    '(all certificates you can find on my LinkedIn page).',
-    '💡 Also, I have few projects in web development on Python and NodeJS.'
+    '<p class="list-lamp-hyphens">💡 Study at the fifth course of <a href="https://kau.org.ua/" target="_blank">KAU</a>, "Computer Science" specialization.</p>',
+    '<p class="list-lamp-hyphens">💡 Graduated with bachelor`s degree "Information System and Technologies" specialization, FICT, KPI.</p>',
+    '<p class="list-lamp-hyphens">💡 I have already completed few ML and DL courses in Coursera and Stepik, watched ods.ai and Deep Learning School lectures. Also, I`m developing in Big Data, learning Spark, PySpark, Scala, Kafka and so on.</p>',
+    '<p class="list-lamp-hyphens">(all certificates you can find on my LinkedIn page).</p>',
+    '<p class="list-lamp-hyphens">💡 Also, I have few projects in web development on Python and NodeJS.</p>'
 ];
