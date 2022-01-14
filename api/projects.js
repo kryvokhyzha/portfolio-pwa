c4836381508491a56b8b17699d623c8eadeafa72
kryvokhyzha <kriwohizha@gmail.com>
2022-01-14 22:15:49 +0200

    change deploy configuration

--- api/projects.js
+++ api/projects.js
@@ -3,22 +3,28 @@
 module.exports = async () => [
     '💡 <a href="https://github.com/kryvokhyzha/emotion-swap-project" target="_blank">emotion-swap-project</a>',
     '<p class="list-hyphens">🔹 Language: Python3</p>',
-    '<p class="list-hyphens">🔹 The main idea of this project is a creating web-application for swapping emotion on target image using emotion-vector. For developing I have used FOMM, Clip, StyleGAN and custom Emotion Recognition NN.</p><br>',
+    '<p class="list-hyphens">🔹 The main idea of this project is a creating web-application for swapping emotion' +
+    ' on target image using emotion-vector. For developing I have used FOMM, Clip, StyleGAN and custom Emotion Recognition NN.</p><br>',
     '💡 <a href="https://github.com/kryvokhyzha/bert-for-ukranian-ner" target="_blank">bert-ner-ua</a>',
     '<p class="list-hyphens">🔹 Language: Python3</p>',
-    '<p class="list-hyphens">🔹 This is my bachelor`s project. The main idea of this application is a creating NER application for ukrainian language using BERT. Also, I have researched different techniques for reducing size of neural networks.</p><br>',
+    '<p class="list-hyphens">🔹 This is my bachelor`s project. The main idea of this application is a creating NER' + 
+    ' application for ukrainian language using BERT. Also, I have researched different techniques for reducing size of neural networks.</p><br>',
     '💡 <a href="https://github.com/kryvokhyzha/cellular-samsung-project" target="_blank">voice-quality-analyzer</a>',
     '<p class="list-hyphens">🔹 Language: Python</p>',
-    '<p class="list-hyphens">🔹 This project I made in Samsung R&D Institute Practicum. The main idea is a build analyzer and detector for different type of noise in audio files.</p><br>',
+    '<p class="list-hyphens">🔹 This project I made in Samsung R&D Institute Practicum. The main idea is a build' +
+    ' analyzer and detector for different type of noise in audio files.</p><br>',
     '💡 <a href="https://github.com/fennec-fox-cast-team/fennec-fox-cast" target="_blank">fennec-fox-cast</a>',
     '<p class="list-hyphens">🔹 Type: Team Project</p>',
     '<p class="list-hyphens">🔹 Language: JS, Node.JS</p>',
-    '<p class="list-hyphens">🔹 Fennec Fox is a peer-to-peer application, which allows to communicate via voice or video with your friends.</p><br>',
+    '<p class="list-hyphens">🔹 Fennec Fox is a peer-to-peer application, which allows to communicate via voice' + 
+    ' or video with your friends.</p><br>',
     '💡 <a href="https://github.com/kryvokhyzha/gradient-descent" target="_blank">gradient-descent</a>',
     '<p class="list-hyphens">🔹 Language: Python3</p>',
-    '<p class="list-hyphens">🔹 This is my third course work. The main idea of this application is to implement different modifications of gradient descent, visualize and explain how it works using Streamlit.</p><br>',
+    '<p class="list-hyphens">🔹 This is my third course work. The main idea of this application is to implement' + 
+    ' different modifications of gradient descent, visualize and explain how it works using Streamlit.</p><br>',
     '💡 <a href="https://github.com/kryvokhyzha/remark-notebook-project" target="_blank">remark-notebook-project</a>',
     '<p class="list-hyphens">🔹 Language: Java, TypeScript</p>',
     '<p class="list-hyphens">🔹 Framework: SpringBoot, Angular</p>',
-    '<p class="list-hyphens">🔹 This project allows to create notebooks and notes for them before previous authorization. User can send feedback about application via email and share notebooks with other users.</p><br>'
+    '<p class="list-hyphens">🔹 This project allows to create notebooks and notes for them before previous' + 
+    ' authorization. User can send feedback about application via email and share notebooks with other users.</p><br>'
 ];
