8ec280af8571c4b7f67bbbea143b0c19a56a389c
kryvokhyzha <kriwohizha@gmail.com>
2022-01-15 15:48:37 +0200

    edit about page

--- api/about.js
+++ api/about.js
@@ -3,7 +3,7 @@
 module.exports = async () => [
     '<p class="list-lamp-hyphens">💡 Machine Learning Engineer at Kyivstar.</p>',
     '<p class="list-hyphens">🔹 build antifraud and scoring models for financial organizations</p>',
-    '<p class="list-hyphens">🔹 develop traffic cannibalization forecasting model using ML algorithms and Graph Neural Network</p>',
+    '<p class="list-hyphens">🔹 develop traffic cannibalization forecasting model using ML algorithms and Graph Neural Networks</p>',
     '<p class="list-hyphens">🔹 develop custom automl tool</p>',
     '<p class="list-hyphens">🔹 work with a huge amount of data</p>',
     '<p class="list-lamp-hyphens">💡 Big Data Analyst at Kyivstar (from 17-02-2020 till 01-06-2021).</p>',
@@ -15,7 +15,6 @@ module.exports = async () => [
     '<p class="list-lamp-hyphens">💡 Study at the fifth course of <a href="https://kau.org.ua/" target="_blank">KAU</a>, "Computer Science" specialization.</p>',
     '<p class="list-lamp-hyphens">💡 Graduated with bachelor`s degree "Information System and Technologies" specialization, FICT, KPI.</p>',
     '<p class="list-lamp-hyphens">💡 I have already completed few ML and DL courses in Coursera and Stepik, watched ods.ai and Deep Learning School lectures.' +
-    ' Also, I`m developing in Big Data, learning Spark, PySpark, Scala, Kafka and so on.</p>',
-    '<p class="list-lamp-hyphens">(all certificates you can find on my LinkedIn page).</p>',
+    ' Also, I`m developing in Big Data, learning Spark, PySpark, Scala, Kafka and so on. (all certificates you can find on my LinkedIn page)</p>',
     '<p class="list-lamp-hyphens">💡 Also, I have few projects in web development on Python and NodeJS.</p>'
 ];
