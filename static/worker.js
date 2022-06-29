00531e740d552b855b9b58032166063720f7fbfa
kryvokhyzha <kriwohizha@gmail.com>
2022-06-29 12:34:55 +0300

    bug fix; deploy new version

--- static/worker.js
+++ static/worker.js
@@ -11,7 +11,7 @@ const files = [
     '/Roman.png'
 ];
 
-const cacheName = 'v1.5.2.0';
+const cacheName = 'v1.5.3.0';
 
 self.addEventListener('install', event => event.waitUntil(
     caches.open(cacheName).then(cache => cache.addAll(files))
